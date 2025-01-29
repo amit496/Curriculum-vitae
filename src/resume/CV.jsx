1ab0d6edc458237c7d9e8f5b27af89ab8b718207
Rays Coding <gautamamit557@gmail.com>
2025-01-29 22:28:09 +0530

    update

--- src/resume/CV.jsx
+++ src/resume/CV.jsx
@@ -9,8 +9,8 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 let CV = () => {
     return (
         <>
-            <div className='w-[70%] m-auto mt-11 p-6 shadow-lg mb-6'>
-                <header className='flex justify-between items-start'>
+            <div className='w-full md:w-[70%] m-auto mt-11 p-6 shadow-lg mb-6'>
+                <header className='flex flex-col md:flex-row justify-between items-start'>
                     <div className='flex items-center'>
                         <div className='border-3 border-gray-500 rounded-full p-2'>
                             <img
@@ -20,12 +20,12 @@ let CV = () => {
                             />
                         </div>
                         <div className='ml-4'>
-                            <h1 className='text-4xl font-bold'>AMIT</h1>
-                            <h1 className='text-4xl font-bold'>Gautam</h1>
+                            <h1 className='text-2xl md:text-4xl font-bold'>AMIT</h1>
+                            <h1 className='text-2xl md:text-4xl font-bold'>Gautam</h1>
                         </div>
                     </div>
 
-                    <div className='flex flex-col ml-8 space-y-3'>
+                    <div className='flex flex-col mt-4 md:mt-0 ml-0 md:ml-8 space-y-3'>
                         <div className='flex items-center'>
                             <PhoneIcon className='mr-2' />
                             <span>+91 8573965259</span>
@@ -54,7 +54,7 @@ let CV = () => {
                     <hr className='my-2 border-t-2 border-gray-300' />
                     <div className='mt-2'>
                         <div className='flex justify-between'>
-                            <h2 className='text-l'>Experienced Laravel Developer with expertise in PHP and web application development. Skilled in optimizing performance, delivering scalable solutions, and collaborating with teams to enhance user experience. Focused on writing clean, efficient code and meeting project deadlines.</h2>
+                            <h2 className='text-base md:text-l'>Experienced Laravel Developer with expertise in PHP and web application development. Skilled in optimizing performance, delivering scalable solutions, and collaborating with teams to enhance user experience. Focused on writing clean, efficient code and meeting project deadlines.</h2>
                         </div>
                     </div>
                 </div>
@@ -65,14 +65,14 @@ let CV = () => {
                     <hr className='my-2 border-t-2 border-gray-300' />
                     <div className='mt-2'>
                         <div className='flex justify-between'>
-                            <h2 className='text-l'>Diploma in Information Technology</h2>
-                            <h1 className="text-l font-semibold">2019 - 2022</h1>
+                            <h2 className='text-base md:text-l'>Diploma in Information Technology</h2>
+                            <h1 className="text-base md:text-l font-semibold">2019 - 2022</h1>
                         </div>
                         <p className='mt-1'>Hewett Polytechnic, Lucknow</p>
                     </div>
                 </div>
 
-                {/* taraiing Section */}
+                {/* Trainings Section */}
                 <div className='education mt-3'>
                     <h1 className='text-xl font-semibold'>Trainings & Certifications</h1>
                     <hr className='my-2 border-t-2 border-gray-300' /> {/* Horizontal Line */}
@@ -125,7 +125,7 @@ let CV = () => {
                     </div>
                 </div>
 
-                {/* taraiing Section */}
+                {/* Area of Interest Section */}
                 <div className="areaofinterest mt-3">
                     <h1 className="text-xl font-semibold">Area of Interest</h1>
                     <hr className="my-2 border-t-2 border-gray-300" /> {/* Horizontal Line */}
@@ -144,8 +144,8 @@ let CV = () => {
                     <hr className="my-2 border-t-2 border-gray-300" />
                     <div className="mt-2">
                         <div className='flex justify-between items-center'>
-                            <h1 className="text-l font-semibold">Laravel Developer</h1>
-                            <h1 className="text-l font-semibold">May 2024 - Present</h1>
+                            <h1 className="text-base md:text-l font-semibold">Laravel Developer</h1>
+                            <h1 className="text-base md:text-l font-semibold">May 2024 - Present</h1>
                         </div>
                         <div>
                             <h1 className='font-semibold'>Singsys Pte Ltd</h1>
@@ -154,8 +154,8 @@ let CV = () => {
                     </div>
                     <div className="mt-3">
                         <div className='flex justify-between items-center'>
-                            <h1 className="text-l font-semibold">Laravel Developer</h1>
-                            <h1 className="text-l font-semibold">Mar 2024 - Apr 2024</h1>
+                            <h1 className="text-base md:text-l font-semibold">Laravel Developer</h1>
+                            <h1 className="text-base md:text-l font-semibold">Mar 2024 - Apr 2024</h1>
                         </div>
                         <div>
                             <h1 className='font-semibold'>Inventics Software PVT LTD</h1>
@@ -164,8 +164,8 @@ let CV = () => {
                     </div>
                     <div className="mt-3">
                         <div className='flex justify-between items-center'>
-                            <h1 className="text-l font-semibold">PHP and Laravel Developer</h1>
-                            <h1 className="text-l font-semibold">May 2022 - Feb 2024</h1>
+                            <h1 className="text-base md:text-l font-semibold">PHP and Laravel Developer</h1>
+                            <h1 className="text-base md:text-l font-semibold">May 2022 - Feb 2024</h1>
                         </div>
                         <div>
                             <h1 className='font-semibold'>Quick Infotech Technology PVT Ltd</h1>
@@ -180,7 +180,7 @@ let CV = () => {
                     <hr className="my-2 border-t-2 border-gray-300" />
                     <div className="mt-2">
                         <div className='flex justify-between items-center'>
-                            <h1 className="text-l font-semibold">Aayansh R Health Care</h1>
+                            <h1 className="text-base md:text-l font-semibold">Aayansh R Health Care</h1>
                         </div>
                         <div>
                             <p>In the Ayansh R Health Care project, I developed APIs and managed dashboards, improving user experience and system performance.</p>
@@ -188,10 +188,27 @@ let CV = () => {
                     </div>
                     <div className="mt-3">
                         <div className='flex justify-between items-center'>
-                            <h1 className="text-l font-semibold">NSW Dogs</h1>
+                            <h1 className="text-base md:text-l font-semibold">NSW Dogs</h1>
                         </div>
                         <div>
                             <p>In the “NSW Dogs” project, I modified the CRUD functionality of the form, improving data management and workflow.</p>
+                        </div>
+                    </div>
+                    <div className="mt-3">
+                        <div className='flex justify-between items-center'>
+                            <h1 className="text-base md:text-l font-semibold">Dappr</h1>
+                        </div>
+                        <div>
+                            <p>dappr is an e-commerce project in this project I managed the question screen and managed the booking status and reveal status according to the booking time.</p>
+                        </div>
+                    </div>
+                    <div className="mt-3">
+                        <div className='flex justify-between items-center'>
+                            <h1 className="text-base md:text-l font-semibold">TTG Media </h1>
+                        </div>
+                        <div>
+                            <p>TTG media experiance
+                            In this project I performed CRUD operations by saving data using third party (Globaltics) API. Implemented "FOMO Pay" payment gateway for booking and created API for front end.</p>
                         </div>
                     </div>
                 </div>
